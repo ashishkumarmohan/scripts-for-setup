ae8c6c1b0324e9bf43fd4b118db711676e520b6d
ashishkumarmohan5 <ashishkumarmohan5@gmail.com>
2023-01-23 00:54:21 +0530

    Changes

--- npmconfig.sh
+++ npmconfig.sh
@@ -1,4 +1,6 @@
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash - &&\
+sudo apt-get install -y nodejs
 mkdir ~/.npm-global
 npm config set prefix '~/.npm-global'
-export PATH=~/.npm-global/bin:$PATH
+sh -c 'echo "export PATH=~/.npm-global/bin:$PATH" >>~/.profile'
 source ~/.profile
